2089d93ad26e33523206b2ccb8cae94df87e52fe
Bartroz <rozwadowski512@gmail.com>
2023-10-15 15:00:14 +0200

    refactor: change services  injection

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormValueService } from 'src/app/services/form-value.service';
@@ -10,15 +10,15 @@ import { UsernameService } from 'src/app/services/username.service';
   styleUrls: ['homepage.component.scss'],
 })
 export class HomepageComponent {
+  private userService = inject(UsernameService)
+  private formService = inject(FormValueService)
+  private router = inject(Router)
 
 canNavigate: boolean = true
-
   isEmpty: boolean = false;
 
   constructor(
-    private userService: UsernameService,
-    private formService: FormValueService,
-    private router: Router
+    // private router: Router
   ) {}
 
   goToNextPage(userName: string, formValid: boolean | any): string {
